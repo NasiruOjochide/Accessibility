e12e9b6805c94a81f33449511ac98db0b2167e05
Danjuma Nasiru <talk2danjumanas@gmail.com>
2023-03-30 06:37:43 +0100

    Initial Commit

---
+++ AccessibilityTutorial/AccessibilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilityApp.swift
+//  Accessibility
+//
+//  Created by Danjuma Nasiru on 22/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
